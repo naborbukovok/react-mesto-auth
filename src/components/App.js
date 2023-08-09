bd9e07257c13fd54dc6290d36e27f184bc7838e9
maria <mdmolokova@edu.hse.ru>
2023-08-09 21:35:57 +0300

    Fixed bugs

--- src/components/App.js
+++ src/components/App.js
@@ -52,24 +52,56 @@ function App() {
                 title="Обновить аватар"
                 name="avatar"
                 onClose={closeAllPopups}
-            />
+            >
+                <label className="popup__field">
+                    <input name="avatar" placeholder="Ссылка на картинку" type="url" className="popup__input" id="avatar" required />
+                    <span className="popup__error avatar-error"></span>
+                </label>
+                <button type="submit" className="popup__button">Сохранить</button>
+            </PopupWithForm>
+
             <PopupWithForm
                 isOpen={isEditProfilePopupOpen}
                 title="Редактировать профиль"
                 name="user"
                 onClose={closeAllPopups}
-            />
+            >
+                <label className="popup__field">
+                    <input name="name" placeholder="Имя" type="text" className="popup__input" id="name" required minLength="2" maxLength="40" />
+                    <span className="popup__error name-error"></span>
+                </label>
+                <label className="popup__field">
+                    <input name="description" placeholder="О себе" type="text" className="popup__input" id="description" required minLength="2" maxLength="200"/>
+                    <span className="popup__error description-error"></span>
+                </label>
+                <button type="submit" className="popup__button">Сохранить</button>
+            </PopupWithForm>
+            
             <PopupWithForm
                 isOpen={isAddPlacePopupOpen}
                 title="Новое место"
                 name="place"
                 onClose={closeAllPopups}
-            />
+            >
+                <label className="popup__field">
+                    <input name="title" placeholder="Название" type="text" className="popup__input" id="title" required minLength="2" maxLength="30" />
+                    <span className="popup__error title-error"></span>
+                </label>
+                <label className="popup__field">
+                    <input name="image" placeholder="Ссылка на картинку" type="url" className="popup__input" id="image" required />
+                    <span className="popup__error image-error"></span>
+                </label>
+                <button type="submit" className="popup__button">Создать</button>
+            </PopupWithForm>
+            
             <PopupWithForm
                 title="Вы уверены?"
                 name="confirmation"
                 onClose={closeAllPopups}
-            />
+            >
+                <button type="submit" className="popup__button popup__button_margin-top_small">Да</button>
+            </PopupWithForm>
+            
             <ImagePopup
                 card={selectedCard}
                 onClose={closeAllPopups}
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,51 +4,9 @@ function PopupWithForm(props) {
             <div className="popup__container popup__container_type_form">
                 <button onClick={props.onClose} type="button" aria-label="Закрыть" className="popup__close-button"></button>
                 <h2 className="popup__title">{props.title}</h2>
-
-                {(props.name === "avatar") && (
-                    <form name="form" className="popup__form" noValidate>
-                        <label className="popup__field">
-                            <input name="avatar" placeholder="Ссылка на картинку" type="url" className="popup__input" id="avatar" required />
-                            <span className="popup__error avatar-error"></span>
-                        </label>
-                        <button type="submit" className="popup__button">Сохранить</button>
-                    </form>
-                )}
-
-                {(props.name === "user") && (
-                    <form name="form" className="popup__form" noValidate>
-                        <label className="popup__field">
-                            <input name="name" placeholder="Имя" type="text" className="popup__input" id="name" required minLength="2" maxLength="40" />
-                            <span className="popup__error name-error"></span>
-                        </label>
-                        <label className="popup__field">
-                            <input name="description" placeholder="О себе" type="text" className="popup__input" id="description" required minLength="2" maxLength="200"/>
-                            <span className="popup__error description-error"></span>
-                        </label>
-                        <button type="submit" className="popup__button">Сохранить</button>
-                    </form>
-                )}
-
-                {(props.name === "place") && (
-                    <form name="form" className="popup__form" noValidate>
-                        <label className="popup__field">
-                            <input name="title" placeholder="Название" type="text" className="popup__input" id="title" required minLength="2" maxLength="30" />
-                            <span className="popup__error title-error"></span>
-                        </label>
-                        <label className="popup__field">
-                            <input name="image" placeholder="Ссылка на картинку" type="url" className="popup__input" id="image" required />
-                            <span className="popup__error image-error"></span>
-                        </label>
-                        <button type="submit" className="popup__button">Создать</button>
-                    </form>
-                )}
-
-                {(props.name === "confirmation") && (
-                    <form name="form" className="popup__form" noValidate>
-                        <button type="submit" className="popup__button popup__button_margin-top_small">Да</button>
-                    </form>
-                )}
-                
+                <form name={`form-${props.name}`} className="popup__form" noValidate>
+                    {props.children}
+                </form>
             </div>
         </div>
     );
